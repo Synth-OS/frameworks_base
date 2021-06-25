BlissRoms/platform_frameworks_base

SystemUI: Add center style Typographic clock
* Blissroms edit [@rohanpurohit]: clock kickoff and font and size

Change-Id: I2ac0655599ead37bf735f2ebf545f833ede495bb

packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -163,6 +163,7 @@ public final class ClockManager {
         addBuiltinClock(() -> new SpectrumClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new SpideyClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new TypeClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new TypeClockAltController(res, layoutInflater, colorExtractor));
 
         // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();

Synth-OS/frameworks_base

Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -153,6 +153,7 @@ public final class ClockManager {
         addBuiltinClock(() -> new BubbleClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new AnalogClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new TypeClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new TypeClockCenteredController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new IDEClockController(res, layoutInflater, colorExtractor, context));
         addBuiltinClock(() -> new DividedLinesClockController(res, layoutInflater, colorExtractor));
 