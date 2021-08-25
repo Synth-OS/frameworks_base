cbb905d6eadaa22d327be1dd7c0de9920b6fcf60
IacobIonut01 <paulionut2003@gmail.com>
2021-08-25 18:10:47 +0000

    FOD: Make background transparent and remove unused res
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>
    Change-Id: Idb0c722fbbace5132ecfb6be5708a92e2659e372
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/FODIconView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODIconView.java
@@ -172,8 +172,6 @@ public class FODIconView extends ImageView implements OmniSettingsService.OmniSe
         this.mIsKeyguard = z;
         if (this.mIsKeyguard || !this.mIsFODIconAnimated) {
             setBackgroundTintList(null);
-        } else {
-            setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#807B7E")));
         }
     }
 }
